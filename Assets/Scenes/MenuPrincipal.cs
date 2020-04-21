cd91a77d04a81977747fa891ce8c8111a517485e
THESERGIO2699 <sergioaln2017@gmail.com>
2020-04-21 16:47:02 -0500

    menus en desarrollo

---
+++ Assets/Scenes/MenuPrincipal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void EmpezarNuevoJuego()
+    {
+        SceneManager.LoadScene("SelectCharacter", LoadSceneMode.Single);
+    }
+    public void Salir()
+    {
+        Application.Quit();
+        Debug.Log("Saliendo");
+    }
+}
