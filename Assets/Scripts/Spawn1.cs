9f52ae6c327f7b3c1e01093acae9b1fad78ca3ce
CarlitosAlegira <lolita3618@outlook.es>
2020-04-24 01:26:17 -0500

    Arreglos Varios

--- Assets/Scripts/Spawn1.cs
+++ Assets/Scripts/Spawn1.cs
@@ -4,12 +4,32 @@ using UnityEngine;
 
 public class Spawn1 : MonoBehaviour
 {
+    public GameObject part;
+    bool dest;
     GameObject player;
     [SerializeField] Transform spawn;
+    
     private void Awake()
     {
+        dest=false;
         player = GameObject.FindGameObjectWithTag("Player");
         player.transform.position = spawn.position;
         player.transform.rotation = spawn.rotation;
+    }
+    private void Start()
+    {
+        GameObject clon= Instantiate(part,transform.position,part.transform.rotation) as GameObject;
+        Destroy(clon, 5);
+        if (dest)
+        {
+            Destroy(clon);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag=="Player")
+        {
+            dest = true;
+        }
     }
 }
