9c56d48173b82b05b497664155d41d1727d9ee14
THESERGIO2699 <sergioaln2017@gmail.com>
2020-04-28 21:11:35 -0500

    ia leñadores

--- Assets/Scripts/IA_Lenador.cs
+++ Assets/Scripts/IA_Lenador.cs
@@ -3,100 +3,95 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Agente : MonoBehaviour
+public class IA_Lenador: MonoBehaviour
 {
-    enum state
+    // Start is called before the first frame update
+    enum Stados
     {
         IDLE, FOLLOW, ATTACK
     }
-
-
-    NavMeshAgent agente;
-    Animator anim;
-    public GameObject heroe;
-    public float distanciaAgente;
-    public float distaciaSeguir;
-    public float distanciaAtaque;
-    private state currentState;
+    Stados currentstate;
+    public Animator anim;
+    NavMeshAgent nav1;
+    public GameObject objetivo;
+    public ParticleSystem Particula;
+    public float disActual, disReferencia, disReferencia2;
     void Start()
     {
-        agente = GetComponent<NavMeshAgent>();
-        heroe = GameObject.FindGameObjectWithTag("Player");
-        currentState = state.IDLE;
+        objetivo = GameObject.FindGameObjectWithTag("Player");
+        nav1 = GetComponent<NavMeshAgent>();
+        disReferencia = 10;
+        disReferencia2 = 1.5f;
+        currentstate = Stados.IDLE;
     }
 
+    // Update is called once per frame
     void Update()
     {
+        behaviour();
         checkConditions();
-        behaivor();
     }
 
-    void behaivor()
+    void checkConditions()
     {
-        switch (currentState)
+        disActual = Vector3.Distance(objetivo.transform.position, transform.position);
+        if (disActual <= disReferencia)
         {
-            case state.IDLE:
+            if (disActual <= disReferencia2)
+            {
+                currentstate = Stados.ATTACK;
+            }
+            else
+            {
+                currentstate = Stados.FOLLOW;
+            }
+        }
+        else
+        {
+            currentstate = Stados.IDLE;
+        }
+    }
+    void behaviour()
+    {
+        switch (currentstate)
+        {
+            case Stados.IDLE:
                 idle();
                 break;
-            case state.FOLLOW:
+            case Stados.FOLLOW:
                 follow();
                 break;
-            case state.ATTACK:
+            case Stados.ATTACK:
                 attack();
                 break;
             default:
                 break;
         }
     }
-
-    void checkConditions()
-    {
-        distanciaAgente = Vector3.Distance(heroe.transform.position, transform.position);
-
-        if (distanciaAgente < distaciaSeguir)
-        {
-            if (distanciaAgente < distanciaAtaque)
-            {
-                currentState = state.ATTACK;
-            }
-            else
-            {
-                currentState = state.FOLLOW;
-            }
-        }
-        else
-        {
-            currentState = state.IDLE;
-        }
-    }
-
     void idle()
     {
-        anim.SetBool("idleLenador", true);
-
-        anim.SetBool("correrLenador", false);
-        anim.SetBool("ataqueLenador", false);
-        agente.SetDestination(transform.position);
-        
+        anim.SetBool("Quieto", true);
+        anim.SetBool("Camina", false);
+        anim.SetBool("Ataque", false);
+        Debug.Log("quieto");
+        nav1.SetDestination(transform.position);
     }
-
     void follow()
     {
-        anim.SetBool("correrLenador", true);
-
-        anim.SetBool("idleLenador", false);
-        anim.SetBool("ataqueLenador", false);
-        agente.SetDestination(heroe.transform.position);
+        Debug.Log("seguir");
+        anim.SetBool("Quieto", false);
+        anim.SetBool("Camina", true);
+        anim.SetBool("Ataque", false);
+        nav1.SetDestination(objetivo.transform.position);
     }
-
     void attack()
     {
-        anim.SetBool("ataqueLenador", true);
-
-        anim.SetBool("idleLenador", false);
-        anim.SetBool("correrLenador", false);
-        agente.SetDestination(transform.position);
-
+        Debug.Log("atacar");
+        anim.SetBool("Quieto", false);
+        anim.SetBool("Camina", false);
+        anim.SetBool("Ataque", true);
+        nav1.SetDestination(transform.position);
     }
+    
 
 }
