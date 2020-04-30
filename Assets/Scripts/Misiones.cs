6dd628d2855fbd0b640d44cc80dfa4c4d342b411
CarlitosAlegira <lolita3618@outlook.es>
2020-04-30 03:53:21 -0500

    Misiones4

--- Assets/Scripts/Misiones.cs
+++ Assets/Scripts/Misiones.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Misiones : MonoBehaviour
 {
+    public int mision;
     private void OnTriggerStay(Collider other)
     {
         if (other.tag=="Player")
@@ -16,12 +17,27 @@ public class Misiones : MonoBehaviour
                     other.GetComponent<Inventario>().menus2 = true;
                     Cursor.visible = true;
                     Cursor.lockState = CursorLockMode.None;
-                    GameObject.Find("Canvas_base").GetComponent<Canvas_jugador>().C_text.text = "Hey veo que has vuelto me alegra mucho.\n Pero bueno vamos al grano. \n \n Hace ya algun tiempo hemos visto que el tamaño de nuestro bosque ha disminuido considerablemente por culpa de unos peligrosos leñadore. \n \n Ve e inestiga y lebera el bosque de esos terribles leñadores";
+                    GameObject.Find("Canvas_base").GetComponent<Canvas_jugador>().C_text.text = "Hey veo que has vuelto me alegra mucho.\n Pero bueno vamos al grano. \n \n Hace ya algun tiempo hemos visto que el tamaño de nuestro bosque ha disminuido considerablemente por culpa de unos peligrosos leñadores. \n \n Ve e investiga y libera el bosque de esos terribles leñadores";
                     GameObject.Find("Canvas_base").GetComponent<Canvas_jugador>().C_mision.SetActive(true);
                     GameObject.Find("Canvas_base").GetComponent<Canvas_jugador>().C_mision.SetActive(true);
                     GameObject.Find("Canvas_base").GetComponent<Canvas_jugador>().barra_vida.SetActive(false);
                     GameObject.Find("Canvas_base").GetComponent<Canvas_jugador>().showarm.SetActive(false);
                     GameObject.FindGameObjectWithTag("Player").GetComponent<Inventario>().des.SetActive(true);
+                    mision = 1;
+                }
+                if (gameObject.name=="mision2")
+                {
+                    other.GetComponent<Movimeinto>().menu = true;
+                    other.GetComponent<Inventario>().menus2 = true;
+                    Cursor.visible = true;
+                    Cursor.lockState = CursorLockMode.None;
+                    GameObject.Find("Canvas_base").GetComponent<Canvas_jugador>().C_text.text = "Hey hola veo que ya has liberado nuestros bosque de esos malvados leñadores.\n Pero bueno tu labor aun no esta terminada. \n \n hemos visto merodeando unos cazadores y nos dimos cuenta de que nuestros animales han desaparecido investiga la zona de las montañas y liberalos";
+                    GameObject.Find("Canvas_base").GetComponent<Canvas_jugador>().C_mision.SetActive(true);
+                    GameObject.Find("Canvas_base").GetComponent<Canvas_jugador>().C_mision.SetActive(true);
+                    GameObject.Find("Canvas_base").GetComponent<Canvas_jugador>().barra_vida.SetActive(false);
+                    GameObject.Find("Canvas_base").GetComponent<Canvas_jugador>().showarm.SetActive(false);
+                    GameObject.FindGameObjectWithTag("Player").GetComponent<Inventario>().des.SetActive(true);
+                    mision = 2;
                 }
             }
         }
@@ -30,7 +46,14 @@ public class Misiones : MonoBehaviour
     {
         salir();
         GameObject.Find("Canvas_base").GetComponent<Canvas_jugador>().Hub_mision.SetActive(true);
-        GameObject.Find("Canvas_base").GetComponent<Canvas_jugador>().C_text2.text="Investiga el Bosque";
+        if (mision == 1)
+        {
+            GameObject.Find("Canvas_base").GetComponent<Canvas_jugador>().C_text2.text = "Investiga el Bosque";
+        }
+        else if (mision == 2)
+        {
+            GameObject.Find("Canvas_base").GetComponent<Canvas_jugador>().C_text2.text = "Investiga las montañas";
+        }
     }
     public void salir()
     {
